4082c5fc419f76f1142cff38d583a59eadd6d595
Caleb Coffin <coffincw@gmail.com>
2019-02-09 18:31:06 -0500

    Update filedit.c

--- filedit.c
+++ filedit.c
@@ -7,6 +7,19 @@ int main(void) {
 	FILE *selectedFile;
 
 	char filename[100], c;
+	while (1) {
+		printf("Enter the name of the file:");
+		scanf("%s", filename);
+
+		selectedFile = fopen(filename, "r");
+		if (selectedFile == NULL) {
+			printf("Cannot open file, please try again");
+		} else {
+			break;
+		}
+	}	
+
+	find()
 
 
 }
@@ -15,7 +28,11 @@ int find(FILE *file, char str[], int size) {
 	char c = fgetc(file);
 	while (c != EOF) {
 		if (c == str[0]) {
-			
+
 		}
 	}
+}
+
+int size(char arr[]) {
+	
 }
