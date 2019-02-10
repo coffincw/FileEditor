6049d22879c28a4d03d6de7ad928ac4323a64567
Caleb Coffin <coffincw@gmail.com>
2019-02-10 16:05:13 -0500

    fixed errors

--- filedit.c
+++ filedit.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define MAX_INPUT_LIMIT 20
 
-int find(FILE *file, char str[]); //returns number of times phrase appears in file
+int find(FILE *file, char str[], int size); //returns number of times phrase appears in file
 int menu(); //returns the number of the selected function
 int size(char arr[]); //takes in a character array and returns the size
 
@@ -25,19 +26,20 @@ int main(void) {
 	while (1) {
 		int selection = menu();
 		switch(selection) {
-			case 1:
+			case 1:;
+			char phrase[4000];
 			while (1) {
-				char phrase[4000];
+				
 				printf("What phrase would you like to search the file for? Please limit your phrase to 20 characters\n");
     			if (fgets(phrase, sizeof(phrase), stdin) == 0)
         			break;  // EOF or error
-    			size_t len = strlen(input);
+    			size_t len = strlen(phrase);
     			if (len > 0)
-        			input[--len] = '\0';   // Zap newline
+        			phrase[--len] = '\0';   // Zap newline
     			if (len <= 20)
         			break;
        	 	}
-       	 	find(selectedFile, phrase[], size(phrase[]));
+       	 	find(selectedFile, phrase, size(phrase));
 		}
 	}
 
@@ -46,8 +48,7 @@ int main(void) {
 int menu() {
 	while (1) {
 		int input = 0;
-		printf("What would you like to do with the file? Enter a number:\n
-			1) find phrase")
+		printf("What would you like to do with the file? Enter a number:\n1) find phrase");
 		scanf(" %i", &input);
 		if (input == 1 || input == 2) {
 			return input;
